5b3457ea61967953cf1ac437504b92ff04773bf9
Sintu Bhatharobhas <osk117@hotmail.com>
2018-05-07 12:56:49 +0700

    revise setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from distutils.core import setup
  
 setup(name='infra_network',
       version='1.0',
-      description='Tool for network testing tools',
+      description='Tool for network testing services',
       author='Sintu Bhatharobhas',
-      author_email='sintu@tisco.co.th',
+      author_email='osk117@hotmail.com',
+      url='https://github.com/osk117/infra_network',
+      download_url='https://github.com/osk117/infra_network',
       packages=['infra_network'],
       license="Sintu Bhatharobhas",
      )
+
